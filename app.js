4e09f9724e03aa91af328990c51edf838e33fff1
Saya Katafuchi <sk@reiji1020.info>
2018-06-15 22:37:58 +0900

    [update]天気情報取得用のルーティングを追加

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const Wether = require('./routes/wether');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/getWether', Wether);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
